099cbfba6a00af5e427454e17d19b452a5980dc4
Shantnu Kaushal <shantnu.kaushal22@stjohns.edu>
2025-05-17 23:22:11 -0400

    gh

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/StockTracker/',
   plugins: [react()],
   server: {
     port: 3000,
     proxy: {
-      // Proxy /api/v1/* → https://finnhub.io/api/v1/*
       '/api/v1': {
         target: 'https://finnhub.io',
         changeOrigin: true,
